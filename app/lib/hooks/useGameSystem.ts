8445d28ff53d670396f7c7b54a18015c75f137a0
helg40k <helg.store@gmail.com>
2025-08-27 01:48:54 +0300

    Remove nextjs annotations from hooks

--- app/lib/hooks/useGameSystem.ts
+++ app/lib/hooks/useGameSystem.ts
@@ -1,5 +1,3 @@
-"use client";
-
 import { useEffect, useMemo, useState } from "react";
 import { notification } from "antd";
 import { usePathname } from "next/navigation";
--- app/lib/hooks/useUser.ts
+++ app/lib/hooks/useUser.ts
@@ -1,5 +1,3 @@
-"use client";
-
 import { useMemo } from "react";
 import { useSession } from "next-auth/react";
 
