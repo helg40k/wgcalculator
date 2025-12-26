1b3326a13562d3e77d6aa5529eb0c07fc3f1b2cb
helg40k <helg.store@gmail.com>
2025-12-26 19:49:34 +0200

    Update nextjs version

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,20 @@
 // see https://next-auth.js.org/configuration/nextjs
 // see https://nextjs.org/docs/app/building-your-application/routing/middleware
 
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+
+// Export the middleware function as default
+export default withAuth(
+  // `withAuth` augments your `Request` with the user's token.
+  function middleware(req) {
+    // Add any custom middleware logic here if needed
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+  },
+);
 
 export const config = {
   // matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).{1,})"],
